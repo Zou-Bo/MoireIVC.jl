96d60401643288d5af919b1f02cad17f5d2a4816
Bo Zou <zou.bo.phys@gmail.com>
2025-05-29 12:46:10 -0500

    start edge calculation

---
+++ src/content/LL HF EDGE.jl
@@ -0,0 +1,36 @@
+"""
+This module gives the interacting edge calculation with a possible potential V(X)
+and the densities of the two ends may be fixed.
+"""
+#module LLHFEDGE
+    
+
+using MKL
+using LinearAlgebra
+using TensorOperations
+using PhysicalUnits
+using MoireIVC.LLHF
+using MoireIVC.Basics: Form_factor, ql_cross
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#end
+
+
+
