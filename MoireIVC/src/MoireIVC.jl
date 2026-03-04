8efcb6f1d3afc0841cc07836d6d41b1df45eccff
Alice and Bob <zou.bo.phys@gmail.com>
2026-03-04 15:29:21 -0500

    Squashed commit of the following:
    commit 6a058f0b57be57138ecef5f6fdeb77c5ca8bbae1
    Author: Alice and Bob <zou.bo.phys@gmail.com>
    Date:   Wed Mar 4 15:18:40 2026 -0500
    
        update
    
    commit 66bf2335bd4aa0fc6f326688721e6a9b18475870
    Author: Alice and Bob <zou.bo.phys@gmail.com>
    Date:   Tue Feb 10 17:06:39 2026 -0500
    
        update the hf code

---
+++ MoireIVC/src/MoireIVC.jl
@@ -0,0 +1,22 @@
+module MoireIVC
+using MKL
+using CairoMakie
+
+public phase_color
+phase_color = range(Makie.Colors.HSV(0,1,1), stop=Makie.Colors.HSV(360,1,1), length=90);
+
+
+include("content/basicthings.jl")
+include("methods/HartreeFock.jl")
+
+
+include("content/LL HF.jl")
+#using .LLHF
+include("plot/LL HF plot.jl")
+#using .LLHF_Plot
+include("content/LL TDHF.jl")
+#using .LLTDHF
+
+
+
+end
